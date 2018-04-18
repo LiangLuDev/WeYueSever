14988a7baf223e34f16e9a0af230d680d8504eb1
LiangLuDev <LiangLuDev@gmail.com>
2018-04-18 08:23:32 +0800

    安装包更新

--- controller/api/UserController.js
+++ controller/api/UserController.js
@@ -523,7 +523,7 @@ class UserController {
      */
     appUpdate(req, res, next) {
         let updateInfo = {
-            versioncode: 3,//版本号
+            versioncode: 4,//版本号
             downloadurl: '/apk/WeYue.apk',//下载链接
         }
         res.json({
